21e934be73dc8405bbdb6ea8b623df77125f251d
Hridesh-Srivastava <hrideshsrivastavaa@outlook.com>
2025-04-21 23:37:11 +0530

    feat: Set Content-Type header to application/json in CORS middleware response

--- middleware.ts
+++ middleware.ts
@@ -23,6 +23,7 @@ export function middleware(request: NextRequest) {
     response.headers.set("Access-Control-Allow-Origin", "*")
     response.headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
     response.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+    response.headers.set("Content-Type", "application/json")
 
     // Return response
     return response
